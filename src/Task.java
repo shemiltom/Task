ce0bc9e5022c3aa6cacf36126d7d6364dc3d976d
Shemil Tom <shemiltom2@gmail.com>
2023-12-07 14:46:49 +0530

    Task management

---
+++ src/Task.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Task {
+    private String description;
+    private boolean complete;
+
+    public Task(String description){
+        this.description=description;
+        this.complete=false;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public boolean isComplete(){
+        return complete;
+    }
+    public void markAsComplete(){
+        this.complete=true;
+    }
+}
+class TaskManager {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    public void addTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        System.out.println("Task added successfully!");
+    }
+
+    public void viewTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks found.");
+        } else {
+            System.out.println("\nTask List:");
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                String status = task.isComplete() ? "Complete" : "Incomplete";
+                System.out.println((i + 1) + ". " + task.getDescription() + " - " + status);
+            }
+        }
+    }
+
+    public void markAsComplete(int taskIndex) {
+        if (taskIndex < 1 || taskIndex > tasks.size()) {
+            System.out.println("Invalid task index.");
+        } else {
+            Task task = tasks.get(taskIndex - 1);
+            task.markAsComplete();
+            System.out.println("Task marked as complete!");
+        }
+    }
+}
---
+++ src/TaskManagementSystem.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class TaskManagementSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TaskManager taskManager = new TaskManager();
+
+        while (true) {
+            System.out.println("\nTask Management System\n");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Mark as Complete");
+            System.out.println("0. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter task description: ");
+                    scanner.nextLine(); // Consume the newline character
+                    String taskDescription = scanner.nextLine();
+                    taskManager.addTask(taskDescription);
+                    break;
+                case 2:
+                    taskManager.viewTasks();
+                    break;
+                case 3:
+                    System.out.print("Enter the number of the task to mark as complete: ");
+                    int taskIndex = scanner.nextInt();
+                    taskManager.markAsComplete(taskIndex);
+                    break;
+                case 0:
+                    System.out.println("Exiting program. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
