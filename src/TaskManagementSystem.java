6c1ed1f97297497e91f94f1b66bf56ec7bb4d4d9
Shemil Tom <shemiltom2@gmail.com>
2023-12-11 09:36:13 +0530

    task_test1

--- src/TaskManagementSystem.java
+++ src/TaskManagementSystem.java
@@ -1,4 +1,6 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class TaskManagementSystem {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,37 +14,44 @@ public class TaskManagementSystem {
             System.out.println("4. Remove Task");
             System.out.println("0. Exit");
 
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
+            try {
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter task description: ");
-                    scanner.nextLine();
-                    String taskDescription = scanner.nextLine();
-                    taskManager.addTask(taskDescription);
-                    break;
-                case 2:
-                    taskManager.viewTasks();
-                    break;
-                case 3:
-                    taskManager.viewTasks();
-                    System.out.print("Enter the number of the task to mark as complete: ");
-                    int taskIndex = scanner.nextInt();
-                    taskManager.markAsComplete(taskIndex);
-                    break;
-                case 4:
-                    System.out.print("Enter the number of the task to be removed: ");
-                    int taskdelIndex = scanner.nextInt();
-                    taskManager.deleteTask(taskdelIndex);
-                    break;
-                case 0:
-                    System.out.println("Exiting program. Goodbye!");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter task description: ");
+                        scanner.nextLine();
+                        String taskDescription = scanner.nextLine();
+                        taskManager.addTask(taskDescription);
+                        break;
+                    case 2:
+                        taskManager.viewTasks();
+                        break;
+                    case 3:
+                        taskManager.viewTasks();
+                        System.out.print("Enter the number of the task to mark as complete: ");
+                        int taskIndex = scanner.nextInt();
+                        taskManager.markAsComplete(taskIndex);
+                        break;
+                    case 4:
+                        taskManager.viewTasks();
+                        System.out.print("Enter the number of the task to be removed: ");
+                        int taskdelIndex = scanner.nextInt();
+                        taskManager.deleteTask(taskdelIndex);
+                        break;
+                    case 0:
+                        System.out.println("Exiting program. Goodbye!");
+                        scanner.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next();
             }
         }
     }
 }
+
